67be2a6a500b9d166cc59bec0150f1e60b7daff7
sozoia <zfzg2232@gmail.com>
2024-07-31 15:43:48 +0300

    pycode style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,19 +8,21 @@ from sys import argv
 
 def get_total(user_tasks):
     ''' caluclate the total of tasks completed'''
-    total,done = 0,0
+    total,  done = 0,   0
     for task in user_tasks:
         total += 1
         if task.get('completed'):
             done += 1
-    return total,done
+    return total,   done
+
 
 def get_name(user_data):
     ''' get the user name'''
     for data in user_data:
-        name =  data.get('name')
+        name = data.get('name')
         if name:
             return name
+
 
 def print_done_tasks(user_tasks):
     ''' print completed tasks'''
@@ -28,20 +30,23 @@ def print_done_tasks(user_tasks):
         if task.get('completed'):
             print(f"\t {task.get('title')}")
 
+
 def main():
+
     ''' main logic'''
     if len(argv) > 1:
+
         id = argv[1]
         url_tasks = 'https://jsonplaceholder.typicode.com/todos'
         url_users = 'https://jsonplaceholder.typicode.com/users'
-        params_tasks = { "userId":id }
-        params_users = { "id": id }
+        params_tasks = {"userId": id}
+        params_users = {"id": id}
 
         tasks = requests.get(url_tasks, params=params_tasks)
         users = requests.get(url_users, params=params_users)
 
         if tasks.status_code == 200 and users.status_code == 200:
-            total,done = get_total(tasks.json())
+            total, done = get_total(tasks.json())
             name = get_name(users.json())
             print(f"Employee {name} is done with tasks({done}/{total}):")
             print_done_tasks(tasks.json())
