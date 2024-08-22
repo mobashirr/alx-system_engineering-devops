1d66368a144d11616ce05188ef43de2dbd0c82f7
sozoia <zfzg2232@gmail.com>
2024-08-22 21:55:00 +0300

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,19 @@
 
 import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
-
 
 def top_ten(subreddit):
-    """method doc"""
+    """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if not subreddit or type(subreddit) is not str:
+        return None
     if response.status_code == 200:
         data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
+        posts = data.get("data", {}).get("children", [])
+        if posts:
+            for post in posts:
+                print(post.get("data", {}).get("title"))
     else:
-        print("None")
+        print('None')
